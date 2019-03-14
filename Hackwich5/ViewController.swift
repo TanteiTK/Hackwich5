d0d167d6c6acc0f6f6f7207bf4115486e12c2b33
CM Student <cmstudent@UHWO-ACM-C-01.local>
2019-03-14 10:43:20 -1000

    Not finish yet 3/14/2019

--- Hackwich5/ViewController.swift
+++ Hackwich5/ViewController.swift
@@ -13,9 +13,16 @@ class ViewController: UIViewController {
     @IBOutlet weak var slider: UISlider!
     var currentValue: Int=0
     
+    var targetValue: Int = 0
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+      
         // Do any additional setup after loading the view, typically from a nib.
+   currentValue = lroundf(slider.value)
+        
+        targetValue = 1 + Int(arc4random_uniform(100) )
     }
 
     override func didReceiveMemoryWarning() {
@@ -25,7 +32,7 @@ class ViewController: UIViewController {
 
 
     @IBAction func myGuessButtonPressed(_ sender: UIButton) {
-        let message="The value is:\(currentValue)"
+        let message="The value is:\(currentValue)" + "\nThe target value is : \(targetValue)"
         
         let alert = UIAlertController(title: "Hellow World!", message: message ,preferredStyle:.alert)
         
@@ -33,14 +40,19 @@ class ViewController: UIViewController {
         
         alert.addAction(action)
         
-        present (alert, animated: true, completion:nil)  }
+        present (alert, animated: true, completion:nil)
+        
+    }
     
     
     @IBAction func sliderHasMoved(_ sender: Any) {
         print("The Value of the slider is:\(slider.value)")
       
         currentValue=lroundf(slider.value)
-        
+    } Func startNewRound(){
+    targetValue = 1 + Int (arc4random_uniform(100))
+    currentValue = 50
+    silder.value = currentValue
     }
 }
 
